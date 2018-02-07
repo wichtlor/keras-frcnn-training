14478b103d179354e0668e484e6ecd381a43df4b
Jonas Preckwinkel <jonas.preckwinkel@gmx.net>
2018-02-07 21:21:40 +0100

    asd

--- train_gen.py
+++ train_gen.py
@@ -131,7 +131,7 @@ rpn = nn.rpn(shared_layers, num_anchors, trainable=True)
 classifier = nn.classifier(shared_layers, roi_input, C.num_rois, nb_classes=len(classes_count), trainable=True)
 
 #Instantiierung der Modelle
-model_rpn = Model(img_input, rpn)
+model_rpn = Model(img_input, rpn[:2])
 model_classifier = Model([img_input, roi_input], classifier)
 # this is a model that holds both the RPN and the classifier, used to load/save weights for the models
 model_all = Model([img_input, roi_input], rpn + classifier)
