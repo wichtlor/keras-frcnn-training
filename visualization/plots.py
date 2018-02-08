e5cfda5f4883d0b81b2d852c5ac3405418a477b0
Jonas Preckwinkel <jonas.preckwinkel@gmx.net>
2018-02-08 18:39:48 +0100

    asd

--- visualization/plots.py
+++ visualization/plots.py
@@ -1,21 +1,47 @@
 import matplotlib.pyplot as plt
 import numpy as np
-    
-def plot_loss(train_loss, val_loss, loss_name, path):
+from multiprocessing.dummy import Pool as ThreadPool 
+
+def plot_loss(args):
     fig = plt.subplot()
-    fig.plot(train_loss, label='train')
-    fig.plot(val_loss, label='val')
-    fig.set(xlabel='epoch', ylabel='loss', title=loss_name)
+    fig.plot(args[0], label='train')
+    fig.plot(args[1], label='val')
+    fig.set(xlabel='epoch', ylabel='loss', title=args[2])
     fig.set_ylim(0)    
     fig.legend()
     fig.grid()
-    fig.figure.savefig(path + '{}.png'.format(loss_name))
+    fig.figure.savefig(args[3] + '{}.png'.format(args[2]))
     plt.close()
 
 def save_plots(losses, epoch_num, path):
-    plot_loss(losses[:epoch_num, 0], losses[:epoch_num, 5], 'loss_rpn_cls', path)
-    plot_loss(losses[:epoch_num, 1], losses[:epoch_num, 6], 'loss_rpn_regr', path)
-    plot_loss(losses[:epoch_num, 2], losses[:epoch_num, 7], 'loss_class_cls', path)
-    plot_loss(losses[:epoch_num, 3], losses[:epoch_num, 8], 'loss_class_regr', path)
-    plot_loss(losses[:epoch_num, 4], losses[:epoch_num, 9], 'class_acc', path)
-    plot_loss(np.sum(losses[:epoch_num,:4],axis=1), np.sum(losses[:epoch_num,5:9],axis=1), 'total_loss', path)
+    plot = [
+        [losses[:epoch_num, 0], losses[:epoch_num, 5], 'loss_rpn_cls', path],
+        [losses[:epoch_num, 1], losses[:epoch_num, 6], 'loss_rpn_regr', path],
+        [losses[:epoch_num, 2], losses[:epoch_num, 7], 'loss_class_cls', path],
+        [losses[:epoch_num, 3], losses[:epoch_num, 8], 'loss_class_regr', path],
+        [losses[:epoch_num, 4], losses[:epoch_num, 9], 'class_acc', path],
+        [np.sum(losses[:epoch_num,:4],axis=1), np.sum(losses[:epoch_num,5:9],axis=1), 'total_loss', path]
+    ]    
+    
+    pool = ThreadPool(6)
+    pool.map(plot_loss, plot)
+    pool.close()
+    pool.join()
+
+def test():
+    epoch_num = 1000
+    path = './'
+    losses = np.zeros((2000, 10))
+    plot = [
+        [losses[:epoch_num, 0], losses[:epoch_num, 5], 'loss_rpn_cls', path],
+        [losses[:epoch_num, 1], losses[:epoch_num, 6], 'loss_rpn_regr', path],
+        [losses[:epoch_num, 2], losses[:epoch_num, 7], 'loss_class_cls', path],
+        [losses[:epoch_num, 3], losses[:epoch_num, 8], 'loss_class_regr', path],
+        [losses[:epoch_num, 4], losses[:epoch_num, 9], 'class_acc', path],
+        [np.sum(losses[:epoch_num,:4],axis=1), np.sum(losses[:epoch_num,5:9],axis=1), 'total_loss', path]
+    ]
+    
+    pool = ThreadPool(6)
+    pool.map(plot_loss, plot)
+    pool.close()
+    pool.join()
