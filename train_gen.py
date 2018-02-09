f2324e22d434565a718c54b8794cc079e4992839
Jonas Preckwinkel <jonas.preckwinkel@gmx.net>
2018-02-09 05:00:43 +0100

    asd

--- train_gen.py
+++ train_gen.py
@@ -16,7 +16,7 @@ from keras_frcnn.pascal_voc_parser import get_data
 from keras_frcnn import losses as losses
 from keras_frcnn import roi_helpers as roi_helpers
 
-from visualization.plots import save_plots
+from visualization.plots import save_plots, save_plots_from_history
 from module import data_generators
 
 import tensorflow as tf
@@ -175,11 +175,16 @@ data_gen_cls_train = data_generators.get_classifier_gt(train_imgs, model_rpn, gr
 data_gen_cls_val = data_generators.get_classifier_gt(val_imgs, model_rpn, graph, classes_count, C, nn.get_img_output_length, K.image_dim_ordering(), mode='train')
 rpn_history = []
 classifier_history = []
+
 for epoch_num in range(num_epochs):
-    rpn_history.append(model_rpn.fit_generator(generator=data_gen_train_rpn, steps_per_epoch=2, epochs=1, verbose=1, validation_data=data_gen_val_rpn, validation_steps=1))
-    classifier_history.append(model_classifier.fit_generator(generator=data_gen_cls_train, steps_per_epoch=2, epochs=1, verbose=1, validation_data=data_gen_cls_val, validation_steps=1))
-    print(rpn_history[epoch_num].history)
-    print(classifier_history[epoch_num].history)
+    
+    hist = model_rpn.fit_generator(generator=data_gen_train_rpn, steps_per_epoch=2, epochs=1, verbose=1, validation_data=data_gen_val_rpn, validation_steps=1)
+    rpn_history.append(hist.history)
+    
+    hist = model_classifier.fit_generator(generator=data_gen_cls_train, steps_per_epoch=2, epochs=1, verbose=1, validation_data=data_gen_cls_val, validation_steps=1)
+    classifier_history.append(hist.history)
+    
+    save_plots_from_history(rpn_history, classifier_history, C.model_path)
 
 #==============================================================================
 # 
--- visualization/plots.py
+++ visualization/plots.py
@@ -21,3 +21,36 @@ def save_plots(losses, epoch_num, path):
     plot_loss(losses[:epoch_num, 3], losses[:epoch_num, 8], 'loss_class_regr', path)
     plot_loss(losses[:epoch_num, 4], losses[:epoch_num, 9], 'class_acc', path)
     plot_loss(np.sum(losses[:epoch_num,:4],axis=1), np.sum(losses[:epoch_num,5:9],axis=1), 'total_loss', path)
+    
+def save_plots_from_history(rpn_hist, cls_hist, path):
+    losses = np.zeros((len(rpn_hist), 14))
+    epoch_num = len(rpn_hist)
+    
+            
+    for epoch_num in range(len(rpn_hist)):
+        losses[epoch_num, 0] = rpn_hist[epoch_num]['rpn_out_class_loss']
+        losses[epoch_num, 1] = rpn_hist[epoch_num]['rpn_out_regress_loss']
+        losses[epoch_num, 5] = rpn_hist[epoch_num]['val_rpn_out_class_loss']
+        losses[epoch_num, 6] = rpn_hist[epoch_num]['val_rpn_out_regress_loss']
+        losses[epoch_num, 10] = rpn_hist[epoch_num]['loss']
+        losses[epoch_num, 11] = rpn_hist[epoch_num]['val_loss']
+        
+    for epoch_num in range(len(cls_hist)):
+        losses[epoch_num, 2] = cls_hist[epoch_num]['dense_class_21_loss']
+        losses[epoch_num, 3] = cls_hist[epoch_num]['dense_regress_21_loss']
+        losses[epoch_num, 4] = cls_hist[epoch_num]['dense_class_21_acc']
+        losses[epoch_num, 7] = cls_hist[epoch_num]['val_dense_class_21_loss']
+        losses[epoch_num, 8] = cls_hist[epoch_num]['val_dense_regress_21_loss']
+        losses[epoch_num, 9] = cls_hist[epoch_num]['val_dense_class_21_acc']
+        losses[epoch_num, 12] = cls_hist[epoch_num]['loss']
+        losses[epoch_num, 13] = cls_hist[epoch_num]['val_loss']
+        
+        
+    plot_loss(losses[:epoch_num, 0], losses[:epoch_num, 5], 'loss_rpn_cls', path)
+    plot_loss(losses[:epoch_num, 1], losses[:epoch_num, 6], 'loss_rpn_regr', path)
+    plot_loss(losses[:epoch_num, 2], losses[:epoch_num, 7], 'loss_class_cls', path)
+    plot_loss(losses[:epoch_num, 3], losses[:epoch_num, 8], 'loss_class_regr', path)
+    plot_loss(losses[:epoch_num, 4], losses[:epoch_num, 9], 'class_acc', path)
+    plot_loss(losses[:epoch_num, 10], losses[:epoch_num, 11], 'rpn_loss', path)
+    plot_loss(losses[:epoch_num, 12], losses[:epoch_num, 13], 'detektor_loss', path)
+    plot_loss(losses[:epoch_num, 10]+losses[:epoch_num, 12], losses[:epoch_num, 11]+losses[:epoch_num, 13], 'total_loss', path)
