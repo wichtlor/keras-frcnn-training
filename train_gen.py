f7371f4a11fac75bd3b3af29d55636ecdbc3def9
Jonas Preckwinkel <jonas.preckwinkel@gmx.net>
2018-02-09 03:10:00 +0100

    asd

--- train_gen.py
+++ train_gen.py
@@ -174,11 +174,11 @@ graph = K.get_session().graph
 data_gen_cls_train = data_generators.get_classifier_gt(train_imgs, model_rpn, graph, classes_count, C, nn.get_img_output_length, K.image_dim_ordering(), mode='train')
 data_gen_cls_val = data_generators.get_classifier_gt(val_imgs, model_rpn, graph, classes_count, C, nn.get_img_output_length, K.image_dim_ordering(), mode='train')
 for iteration in range(40):
-    model_rpn.fit_generator(generator=data_gen_train_rpn, steps_per_epoch=5, epochs=1, verbose=1, validation_data=data_gen_val_rpn, validation_steps=200)
+    model_rpn.fit_generator(generator=data_gen_train_rpn, steps_per_epoch=5, epochs=1, verbose=1, validation_data=data_gen_val_rpn, validation_steps=5)
     #model_rpn.save(C.model_path + model_name)
     #model_all.save_weights(C.model_path + model_name)
 
-    model_classifier.fit_generator(generator=data_gen_cls_train, steps_per_epoch=5, epochs=1, verbose=1, validation_data=data_gen_cls_val, validation_steps=200)
+    model_classifier.fit_generator(generator=data_gen_cls_train, steps_per_epoch=5, epochs=1, verbose=1, validation_data=data_gen_cls_val, validation_steps=5)
 
 
 #==============================================================================
