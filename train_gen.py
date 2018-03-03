4ab361738680d0151e4a1f22c36c5b6c18f0d8a7
Jonas Preckwinkel <jonas.preckwinkel@gmx.net>
2018-03-03 02:54:12 +0100

    asd

--- train_gen.py
+++ train_gen.py
@@ -207,7 +207,6 @@ try:
 
         #Trainiere RPN und Classifier im Wechsel fuer je eine Epoche solang EarlyStopping das Training nicht beendet hat
         if wait < patience:
-            rpn_history[epoch_num]['val_loss'][0]
 
             rpn_hist = model_rpn.fit_generator(generator=data_gen_train_rpn, steps_per_epoch=epoch_length, epochs=1, verbose=1, validation_data=data_gen_val_rpn, validation_steps=validation_length, use_multiprocessing=False, workers=2)
             rpn_history.append(rpn_hist.history)
