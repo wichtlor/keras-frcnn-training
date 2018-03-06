18358783a35e4f41257e0dc47d977deaca1423b5
Jonas Preckwinkel <jonas.preckwinkel@gmx.net>
2018-03-06 21:57:38 +0100

    asd

--- netze/vgg16_dense_low.py
+++ netze/vgg16_dense_low.py
@@ -52,7 +52,7 @@ def rpn(base_layers, num_anchors, trainable=False):
     dann Objectness Scores und BBox Regression stattfinden.
     '''
     
-    x = Conv2D(512, (3, 3), kernel_initializer='he_normal', padding='same', activation='relu', kernel_initializer='normal', name='rpn_conv1')(base_layers)
+    x = Conv2D(512, (3, 3), kernel_initializer='he_normal', padding='same', activation='relu', name='rpn_conv1')(base_layers)
 
     x_class = Conv2D(num_anchors, (1, 1), activation='sigmoid', kernel_initializer='uniform', name='rpn_out_class')(x)
     x_regr = Conv2D(num_anchors * 4, (1, 1), activation='linear', kernel_initializer='zero', name='rpn_out_regress')(x)
