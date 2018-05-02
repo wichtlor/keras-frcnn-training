71be2614cf18264ef64b010f99c87c6531206835
Jonas Preckwinkel <jonas.preckwinkel@gmx.net>
2018-05-02 15:19:54 +0200

    asd

--- xor_demo.py
+++ xor_demo.py
@@ -8,6 +8,10 @@ model.add(Dense (1, activation='sigmoid'))
 
 model.compile(optimizer='sgd', loss='binary_crossentropy')
 
+X = np. array ([[0,0],[0,1],[1,0],[1,1]])
+y = np. array ([[0],[1],[1],[0]])
+model.fit(X, y, epochs = 10000 )
+
 results = model.predict(X)
 print(results)
 #[[0. 03734707 ][0. 93873811 ][0. 93955719 ][0. 03086801 ]]
